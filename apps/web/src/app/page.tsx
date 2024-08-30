0a23354ccd23d654451f64c886c0e2fd2b20a695
Isaac Dyor <isaac@dyor.com>
2024-08-29 20:17:07 -0700

    fixing env variables

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { Button } from "@repo/ui/components/ui/button";
-import { db } from "@repo/db/src/index";
-import { users } from "@repo/db/src/schema";
 import { env } from "@/env";
 
 export default function Page() {
   console.log(env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
   return (
     <div>
-      {" "}
       <p>hello</p>
       <p>{env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</p>
     </div>
