0f7547bc29282f2ff350b1077e17c265af56c044
Isaac Dyor <isaac@dyor.com>
2024-08-29 20:00:41 -0700

    removing docs

--- apps/docs/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@repo/ui/components/ui/button";
-
-export default function Page() {
-  return (
-    <main>
-      <Button className="bg-red-500">Click me</Button>
-    </main>
-  );
-}
