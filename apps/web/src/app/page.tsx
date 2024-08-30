af5a69e94880fad25f28ada009cccba9b78283ad
Isaac Dyor <isaac@dyor.com>
2024-08-29 19:58:00 -0700

    checking gitignore

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@repo/ui/components/ui/button";
+import { db } from "@repo/db/src/index";
+import { users } from "@repo/db/src/schema";
+import { env } from "@/env";
+
+export default function Page() {
+  console.log(env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  return (
+    <div>
+      {" "}
+      <p>{env.NEXT_PUBLIC_SUPABASE_ANON_KEY}</p>
+    </div>
+  );
+}
